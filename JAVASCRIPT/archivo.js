95b8ccc899d6e25714a4da09eaadb05002e04d01
alejandro-dev-2111 <personalalejandro2111@gmail.com>
2026-04-30 22:01:08 -0600

    Agregué el mensaje por correo electronico

--- JAVASCRIPT/archivo.js
+++ JAVASCRIPT/archivo.js
@@ -1,6 +1,39 @@
-const hamburguesa = document.querySelector(".hamburguesa")
-const menu = document.querySelector(".Menu")
+//menu para responsive
+const hamburguesa = document.querySelector(".hamburguesa");
+const menu = document.querySelector(".Menu");
 
-hamburguesa.addEventListener("click", function(){
-    menu.classList.toggle("activo")
-})
+hamburguesa.addEventListener("click", function () {
+  menu.classList.toggle("activo");
+});
+//mensaje por correo
+emailjs.init("2iA66LdavwzSa077Q");
+
+const form = document.querySelector("form");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const nombre = document.getElementById("nombre").value;
+  const telefono = document.getElementById("numero").value;
+  const mensaje = document.getElementById("mensaje").value;
+
+  if (nombre === "" || telefono === "" || mensaje === "") {
+    alert("Por favor llena todos los campos");
+    return;
+  }
+
+  emailjs
+    .send("service_x8vup6w", "template_m92es0r", {
+      nombre: nombre,
+      telefono: telefono,
+      mensaje: mensaje,
+    })
+    .then(function () {
+      alert("¡Mensaje enviado correctamente!");
+      form.reset();
+    })
+    .catch(function (error) {
+      alert("Error al enviar. Intenta de nuevo.");
+      console.log(error);
+    });
+});
